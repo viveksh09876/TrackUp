63fd01f2ad4555188b1ae4241a4d65d4dc08256e
Vivek Sharma <vivek@192.168.1.5>
2025-11-29 20:21:15 +0530

    endpoints created

--- next-app/app/api/auth/[...nextauth]/route.ts
+++ next-app/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,35 @@
+import { prismaClient } from "@/app/lib/db";
+import { Provider } from "@prisma/client";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
+    adapter: undefined,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
         })
-    ]
+    ],
+    callbacks: {
+        async signIn(params) {
+            if (!params.user.email) { 
+                return false
+            }
+            try {
+                await prismaClient.user.create({
+                    data: {
+                        email: params.user.email ?? "",
+                        provider: Provider.Google
+                    }
+                })
+            } catch(e) {
+
+            }
+            
+            return true;
+        }
+    }
 });
 
 export { handler as GET, handler as POST }
